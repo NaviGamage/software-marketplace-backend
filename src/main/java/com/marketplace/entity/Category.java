f861a9e4ae4be53e6e05803c2bca5a4faae9e2f9
NaviGamage <navinduprabashwara@gmail.com.com>
2026-08-23 13:24:32 +0530

    feat: add Category entity

---
+++ src/main/java/com/marketplace/entity/Category.java
@@ -0,0 +1,36 @@
+package com.marketplace.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Generated;
+import org.hibernate.generator.EventType;
+
+import java.time.OffsetDateTime;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, unique = true, length = 100)
+    private String name;
+
+    @Column(name = "slug", nullable = false, unique = true, length = 100)
+    private String slug;
+
+    @Generated(event = EventType.INSERT)
+    @Column(name = "created_at", insertable = false, updatable = false)
+    private OffsetDateTime createdAt;
+}
