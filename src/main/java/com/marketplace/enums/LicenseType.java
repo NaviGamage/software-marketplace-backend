f4e258e2e51f66c2991b2bf001a2f61880cb6a8d
NaviGamage <navinduprabashwara@gmail.com.com>
2026-08-24 18:57:49 +0530

    feat(product): add LicenseType and ProductStatus enums

---
+++ src/main/java/com/marketplace/enums/LicenseType.java
@@ -0,0 +1,6 @@
+package com.marketplace.enums;
+
+public enum LicenseType {
+    REGULAR,
+    EXTENDED
+}
---
+++ src/main/java/com/marketplace/enums/ProductStatus.java
@@ -0,0 +1,11 @@
+package com.marketplace.enums;
+
+public enum ProductStatus {
+    DRAFT,
+    SCANNING,
+    PENDING_REVIEW,
+    APPROVED,
+    REJECTED,
+    SUSPENDED
+
+}
